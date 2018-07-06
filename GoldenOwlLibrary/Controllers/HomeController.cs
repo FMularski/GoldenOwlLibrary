de31c57d20b68a0b79bb010c9d28bd355d0b851b
Filip Mularski <filipelblag@gmail.com>
2018-07-06 11:08:19 +0200

    Log in / log out navbar

--- GoldenOwlLibrary/Controllers/HomeController.cs
+++ GoldenOwlLibrary/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoldenOwlLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,23 @@ namespace GoldenOwlLibrary.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext Context;
+
+        public HomeController()
+        {
+            Context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Context.Dispose();
+        }
+
         public ActionResult Index()
         {
+            if(User.Identity.IsAuthenticated)
+                return View("IndexLogged");
+
             return View();
         }
     }
